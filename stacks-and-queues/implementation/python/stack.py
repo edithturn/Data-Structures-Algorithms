ab3b083586ea327e0fe0f91ab61da06ab2570bfc
edithturn <edithpuclla20@gmail.com>
2020-12-15 00:15:03 -0800

    adding comments to stack implementation

--- stacks-and-queues/implementation/python/stack.py
+++ stacks-and-queues/implementation/python/stack.py
@@ -1,13 +1,20 @@
+# Defining the class for stack
 class Stack:
+	# Define "items" as an empty initial value
 	def __init__(self):
 		self.items = []
+	# Return all the values of items in the list
 	def isEmpty(self):
 		return self.items == []
+	# Add an item on the top of the list
 	def push(self, item):
 		self.items.append(item)
+	# remove an item from the top of the list
 	def pop(self):
 		return self.items.pop()
+	# TODO
 	def peek(self):
 		return self.items[len(self.items) - 1]
+	# Return the length of the items
 	def size(self):
 		return len(self.items)
